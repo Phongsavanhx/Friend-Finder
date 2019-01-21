d830e1ad3eab8b635f9227fd526ddd2071cc706c
Sushi Sama <Phongsavanhx@gmail.com>
2019-01-20 22:00:42 -0700

    first commit

---
+++ app/data/friend.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Kim Jaejoong',
+		photo: 'http://asianwiki.com/File:Kim_Jae-Joong-p3.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Taeyong',
+		photo: 'https://pbs.twimg.com/media/DTYVQgLUQAAPi7O.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Bruce Lee',
+		photo: 'https://en.wikipedia.org/wiki/File:Bruce_Lee_1973.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Zackariah',
+		photo: 'https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Kwon Jiyoung',
+		photo: 'http://images.en.koreaportal.com/data/images/full/33215/g-dragon-shares-his-personal-struggles-of-being-a-top-idol.jpg?w=750',
+		scores: [	
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	
+{
+		name: 'Doublelift',
+		photo: 'https://www.esports.net/wp-content/uploads/2017/05/Doublelift-esports-player-300x238.png',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	
+	{
+		name: 'Beyonce Knowles',
+		photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Adele Adkins',
+		photo: 'https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Aphromoo',
+		photo: 'https://pbs.twimg.com/profile_images/940107163170512896/pQ8DJwsI_400x400.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Elmo',
+		photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
